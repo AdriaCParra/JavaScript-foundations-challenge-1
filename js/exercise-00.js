9408d616973b084b3149172c86ef1d2f8d0c9366
AdriaCParra <Costaadria524@gmail.com>
2023-10-17 20:24:03 +0200

    COMPLETE exercise-00.js

---
+++ js/exercise-00.js
@@ -0,0 +1,24 @@
+   
+   /* Create a constant variable of each of the following primitive types: 
+   
+   - String
+   - Number
+   - Boolean
+   - Null
+   - Undefined
+   */
+
+   const myString = 'Hello World';
+   const myNumber = 42;
+   const myBoolean = true;
+   const myNull = null;
+   const myUndefined = undefined;
+
+   /* Log out each variable to the terminal at the end. */
+
+   console.log({myString});
+   console.log({myNumber});
+   console.log({myBoolean});
+   console.log({myNull});
+   console.log({myUndefined});
+
