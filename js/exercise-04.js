28490676a86292a1e8503e0df847bcee2b665d15
AdriaCParra <Costaadria524@gmail.com>
2023-10-17 21:08:40 +0200

    COMPLETE exercise-04.js

---
+++ js/exercise-04.js
@@ -0,0 +1,20 @@
+//  What would each of these lines of code do:
+
+    // 1. true && console.log("moo");
+    // should print "moo"
+    // Because the first value is true, the second value is evaluated and printed
+
+    // 2. false && console.log("moo moo?")
+    // should print nothing
+    // Because the first value is false, the second value is not evaluated
+
+    // 3. true || console.log("hello friend");
+    // should print nothing
+    // Because the first value is true, the second value is not evaluated
+
+    // 4. false || console.log("bye friend");
+    // should print "bye friend"
+    // Because the first value is false, the second value is evaluated and printed
+
+    // Once you see the results, write in a comment why it works that way
+
