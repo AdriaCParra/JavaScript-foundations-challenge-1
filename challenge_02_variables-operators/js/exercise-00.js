66677b96595e1e1c426d22f50887874e7f74548a
AdriaCParra <Costaadria524@gmail.com>
2023-10-30 20:47:47 +0100

    format code

--- challenge_02_variables-operators/js/exercise-00.js
+++ challenge_02_variables-operators/js/exercise-00.js
@@ -1,5 +1,4 @@
-   
-   /* Create a constant variable of each of the following primitive types: 
+/* Create a constant variable of each of the following primitive types: 
    
    - String
    - Number
@@ -8,17 +7,16 @@
    - Undefined
    */
 
-   const myString = 'Hello World';
-   const myNumber = 42;
-   const myBoolean = true;
-   const myNull = null;
-   const myUndefined = undefined;
+const myString = "Hello World";
+const myNumber = 42;
+const myBoolean = true;
+const myNull = null;
+const myUndefined = undefined;
 
-   /* Log out each variable to the terminal at the end. */
+/* Log out each variable to the terminal at the end. */
 
-   console.log({myString});
-   console.log({myNumber});
-   console.log({myBoolean});
-   console.log({myNull});
-   console.log({myUndefined});
-   
+console.log({ myString });
+console.log({ myNumber });
+console.log({ myBoolean });
+console.log({ myNull });
+console.log({ myUndefined });
